4d6257523e928f0b92c387488cfd187d9ba87063
DJEMELI TSAPI JOSIAS <djemelitsapijosias@gmail.com>
2026-06-16 12:57:18 +0100

    New adjustment

--- backend/app/main.py
+++ backend/app/main.py
@@ -32,10 +32,18 @@ def startup():
 # )
 
 # Configuration CORS 
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
